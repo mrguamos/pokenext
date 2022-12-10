e1b39895a5b7c48d1564bc9750f2cc16d09b6365
Mark Ryan Guamos <ryan.guamos@gmail.com>
2022-12-10 22:27:51 +0800

    cache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,10 +34,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   res,
   query,
 }) => {
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=31536000, stale-while-revalidate=31536000'
-  )
+  res.setHeader('Cache-Control', 'public, s-maxage=31536000')
   const pokemons = await getPokemons(query.page)
   return {
     props: {
